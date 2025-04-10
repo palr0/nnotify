bd8d5f77f3cd131d55e3cc210c0a65e64472d6c9
palr0 <palr0@naver.com>
2025-04-10 10:44:39 +0900

    Update and rename config.js to config.py

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  TOKEN: process.env.TOKEN,
-  JSONBIN_API_KEY: process.env.JSONBIN_API_KEY,
-  JSONBIN_BIN_ID: process.env.JSONBIN_BIN_ID
-};
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+JSONBIN_API_KEY = os.getenv("JSONBIN_API_KEY")
+JSONBIN_BIN_ID = os.getenv("JSONBIN_BIN_ID")
