5997368ec6aa6c13a02e1e5136fb0a5c7e9eef52
palr0 <palr0@naver.com>
2025-04-11 11:19:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-TOKEN = "TOKEN"
-JSONBIN_API_KEY = "JSONBIN_API_KEY"
-JSONBIN_BIN_ID = "JSONBIN_BIN_ID"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+JSONBIN_API_KEY = os.getenv("JSONBIN_API_KEY")
+JSONBIN_BIN_ID = os.getenv("JSONBIN_BIN_ID")
